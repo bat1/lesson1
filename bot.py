72367062d1e5b15b9d4a49b9ac650073ce68398d
Maxim Batrakov <maxim.batrakov@gmail.com>
2016-10-04 13:00:08 +0300

    Работающий бот, умеет производить арифметические операции заданные словами

--- bot.py
+++ bot.py
@@ -2,26 +2,65 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from answers import get_answer, answers
 
 dict_nums_to_words = {'один': '1', 'два': '2', 'три': '3', 'четыре': '4', 
-'пять': '5', 'шесть': '6', 'семь': '7', 'восемь': '8', 'девять': '9', 'десять': '10', 'умножить на': '*', 
-'прибавить': '+', 'плюс': '+', 'минус': '-', 'отнять': '-', 'разделить на': '/', 'делить на': '/'}
+'пять': '5', 'шесть': '6', 'семь': '7', 'восемь': '8', 'девять': '9', 'десять': '10'}
+
+dict_actions = {'умножить': '*', 
+	'прибавить': '+', 'плюс': '+', 'минус': '-', 'отнять': '-', 'разделить': '/', 'делить': '/'}
 
 def start(bot, update):
 	print("Вызван /start")
 	bot.sendMessage(update.message.chat_id, text='Привет, человек! Я бот, который помогает учиться на курсе Learn Python')
 
 def count(bot, update):
-	bot.sendMessage(update.message.chat_id, text=('В фразе' + ' ' + '"' + update.message.text[7:] + '"' + ':' +  ' ' + '{}'.format(count_words(update.message.text[7:]) + ' ' + 'слова'))) #Отвечаем сколько слов в сторке, которую вводит пользователь
+	bot.sendMessage(update.message.chat_id, text=('В фразе' + ' ' + '"' + update.message.text[7:] + '"' + ':' +  ' ' + '{}'.format(count_words(update.message.text[7:])) + ' ' + 'слова')) #Отвечаем сколько слов в сторке, которую вводит пользователь
+
+def icalc(bot, update):
+	print("Вызван /icalc")
+	bot.sendMessage(update.message.chat_id, text='Привет, человек! Я стал еще умнее! \nХочешь узнать сколько будет' + ' '+ '"' + update.message.text[21:] + '"' + '?' + ' ' + '\nОтвечаю: ' + '{}'.format(actions(calc_words(update.message.text[7:])))) #Считаем арифметические операции сказанные фразами вида "сколль будет два плюс три"
 
 def calc(bot, update):
 	print("Вызван /calc")
-	bot.sendMessage(update.message.chat_id, text='Привет, человек! \nЯ бот - калькулятор, делаю облачные вычисления с помощью нейронных сетей, квантовых процессоров и прочей фигни! Хочешь знать сколько будет' + ' ' + '"' + update.message.text[5:] + '"' + '?' +  ' ' + '\nОтвечаю: ' + '{}'.format(calc_num(update.message.text[5:]))) #Считаем арифметические операции
+	bot.sendMessage(update.message.chat_id, text='Привет! Я бот - калькулятор, делаю вычисления с помощью нейронных сетей, квантовых процессоров и прочей фигни! Хочешь знать сколько будет' + ' ' + '"' + update.message.text[5:] + '"' + '?' +  ' ' + 'Отвечаю: ' + str(calc_num(update.message.text[5:]))) #Считаем арифметические операции
 
-#def calc_num(bot, update):
+def calc_words(text):
 
-#def calc_words(text)
-#	text.split
-	#if text in dict_nums_to_words:
-#	number1, number2 = item.get()
+	separated_words = text.split()
+	flatten = []
+
+	for item in separated_words:
+		num = dict_nums_to_words.get(item)
+		if num:
+				flatten.append(num)
+		action = dict_actions.get(item)
+		if action:
+				flatten.append(action)
+	return flatten
+
+def actions(flatten):
+
+	if '+' in flatten:
+		number1 = flatten[0]
+		number2 = flatten[2]
+		sum_numbers = int(number1) + int(number2)
+		return sum_numbers
+
+	if '-' in flatten:
+		number1 = flatten[0]
+		number2 = flatten[2]
+		difference = int(number1) - int(number2)
+		return difference
+
+	if '*' in flatten:
+		number1 = flatten[0]
+		number2 = flatten[2]
+		composition = int(number1) * int(number2)
+		return composition
+
+	if '/' in flatten:
+		number1 = flatten[0]
+		number2 = flatten[2]
+		division = int(number1) / int(number2)
+		return division
 
 def calc_num(text):
 
@@ -63,6 +102,7 @@ def run_bot():
 	dp.add_handler(CommandHandler("start", start))
 	dp.add_handler(CommandHandler("count", count))
 	dp.add_handler(CommandHandler("calc", calc))
+	dp.add_handler(CommandHandler("icalc", icalc))
 	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 	updater.start_polling()
 	updater.idle()
@@ -71,3 +111,5 @@ if __name__ == '__main__':
 
 	run_bot()
 	calc_num(text)
+	calc_words(text)
+	actions(flatten)
--- test1.py
+++ test1.py
@@ -1,26 +1,52 @@
+dict_nums_to_words = {'один': '1', 'два': '2', 'три': '3', 'четыре': '4', 
+'пять': '5', 'шесть': '6', 'семь': '7', 'восемь': '8', 'девять': '9', 'десять': '10'}
+
+dict_actions = {'умножить': '*', 
+	'прибавить': '+', 'плюс': '+', 'минус': '-', 'отнять': '-', 'разделить': '/', 'делить': '/'}
 
 
-print('Строка {}'.format(5)) #Считаем арифметические операции
 
 
-if __name__ == '__main__':
+def calc_words(text):
 
-	print("Hello, world")
-	a = 1
-	b = 2
-	c = a + b
-	print(c)
+	separated_words = text.split()
+	flatten = []
 
-	dict_nums_to_words = {'один': '1', 'два': '2', 'три': '3', 'четыре': '4', 
-	'пять': '5', 'шесть': '6', 'семь': '7', 'восемь': '8', 'девять': '9', 'десять': '10'}
-	dict_operations = {'умножить на': '*', 
-	'прибавить': '+', 'плюс': '+', 'минус': '-', 'отнять': '-', 'разделить на': '/', 'делить на': '/'}
+	for item in separated_words:
+		num = dict_nums_to_words.get(item)
+		if num:
+				flatten.append(num)
+		action = dict_actions.get(item)
+		if action:
+				flatten.append(action)
+	return flatten
 
-	words = 'сколько будет два плюс три'
-	one_word = words[13:].split()
-	for item in one_word:
-		num1, num2 = dict_nums_to_words.get(item)
-		print(num1)
-		print(num2)
-		action = dict_operations.get(item)
-		print(action)
+def actions(flatten):
+
+	if '+' in flatten:
+		number1 = flatten[0]
+		number2 = flatten[2]
+		sum_numbers = int(number1) + int(number2)
+	return sum_numbers
+
+print(calc_words('сколько будет два плюс три'))
+print(actions(['2','+','3']))
+
+
+
+#	if item in separated_words:
+#		num1, num2 = dict_nums_to_words.get(item)
+#		print(num1 + num2)
+#		return summa
+
+#	if item in separated_words:
+#		action = dict_actions.get(item)
+#		print(action)
+#		return action
+
+#	calc_num(text)
+
+
+
+#dict_keys(['плюс', 'разделить', 'умножить на', 'делить', 'прибавить', 'отнять', 'минус'])
+#['сколько', 'будет', 'два', 'плюс', 'три']
